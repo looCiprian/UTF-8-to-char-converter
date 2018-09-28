af5004a57b68471254cd797526522f7616e78d0d
looCiprian <30753137+looCiprian@users.noreply.github.com>
2018-09-28 21:44:55 +0200

    UTF-8 converter

---
+++ utf-8Function.c
@@ -0,0 +1,65 @@
+//
+//  utf-8Function.c
+//
+
+
+#include "Header.h"
+
+
+/**
+ Controlla se è UTF-8
+
+ @param c carattere da controllare
+ @return true se è UTF-8
+ */
+bool isUTF8(char c){
+    
+    if (((c >> 7) & 1)==1) {                        //Controllo se il primo bit è 1
+        return true;
+    }
+    return false;
+    
+}
+
+
+/**
+ Legge caratteri UTF-8
+
+ @param c carattere da leggere
+ @return 1° e 2° byte uniti (leggere relazione per la spiegazione)
+ */
+char readUtf8(char c){
+    
+    char firstByte=c;
+    firstByte &= ~(1 << 7);                     //Imposto a 0 il primo bit
+    firstByte &= ~(1 << 6);                     //Imposto a 0 il secondo bit
+    firstByte=firstByte<<6;                     //Shifto gli ultimi due bit inprima posizione
+    
+    char secondc=getchar();
+    char secondoByte=secondc;
+    secondoByte &= ~(1 << 7);                   //Imposto a 0 il primo bit del secondo byte
+    
+    secondc=firstByte+secondoByte;              //Sommo i byte
+    
+    return secondc;
+
+}
+
+/**
+ Stampa caratteri UTF-8
+
+ @param c carattere da stampare 
+ */
+void printUtf8(char c){
+    char primoByte=0;
+    char secondoByte=0;
+
+    if( ((c >> 6) & 1) ==1)primoByte=-61;
+    if ( ((c >> 6) & 1) ==0)primoByte=-62;
+    
+    secondoByte=c;
+    secondoByte |= 1 << 7;
+    secondoByte &= ~(1 << 6);
+    putchar(primoByte);
+    putchar(secondoByte);
+}
